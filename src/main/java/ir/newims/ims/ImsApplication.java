3d9629b0c1c8185f73cd9c68490766667c1b1c89
zangene <4900474045@g>
2022-02-16 12:26:02 +0330

    init

---
+++ src/main/java/ir/newims/ims/ImsApplication.java
@@ -0,0 +1,13 @@
+package ir.newims.ims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImsApplication.class, args);
+    }
+
+}
