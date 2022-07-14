f349b066c381b2265b77a63a7b0e9399dea642db
dnaiel zangene <46925439+danielzangene@users.noreply.github.com>
2022-07-14 18:16:28 +0430

    comment Personnel entity

--- src/main/java/ir/newims/ims/models/personnel/personnel/Personnel.java
+++ src/main/java/ir/newims/ims/models/personnel/personnel/Personnel.java
@@ -1,9 +1,13 @@
 package ir.newims.ims.models.personnel.personnel;
 
 import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.Table;
 
-@Entity
-@Table(name = "t_Personnel")
-public class Personnel extends User {
+//@Entity
+//@Table(name = "t_Personnel")
+//@Inheritance(strategy = InheritanceType.JOINED)
+public class Personnel /*extends User*/ {
+
 }
